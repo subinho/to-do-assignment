52b841ed6f795f1c7451b30467158e3e6923d4a0
unknown <stepansubrt@icloud.com>
2024-08-14 01:05:58 +0200

    created todo edit view

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,77 @@
+<x-layout>
+    <form method="POST" action="/todo/{{ $todo->id }}">
+        @csrf
+        @method('PATCH')
+
+        <div class="space-y-12">
+            <div class="border-b border-gray-900/10 pb-12">
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm font-medium leading-6 text-gray-900">Title</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input
+                                    type="text"
+                                    name="title"
+                                    id="title"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Shift Leader"
+                                    value="{{ $todo->title }}"
+                                    required>
+                            </div>
+
+                            <x-form-error name="title" />
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="body" class="block text-sm font-medium leading-6 text-gray-900">Body</label>
+                        <div class="mt-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md"
+                            >
+                                <textarea
+                                    type="text"
+                                    name="body"
+                                    id="body"
+                                    rows="10"
+                                    class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                    placeholder="Description"
+{{--                                    value="{{ $todo->body }}"--}}
+                                    required>
+                                    {{$todo->body}}
+                                </textarea>
+                            </div>
+
+                            <x-form-error name="body" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="mt-6 flex items-center justify-between gap-x-6">
+            <div class="flex items-center">
+                <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+            </div>
+
+            <div class="flex items-center gap-x-6">
+                <a href="/todo/{{ $todo->id }}" class="text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+
+                <div>
+                    <button type="submit"
+                            class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                        Update
+                    </button>
+                </div>
+            </div>
+        </div>
+    </form>
+
+    <form method="POST" action="/todo/{{ $todo->id }}" id="delete-form" class="hidden">
+        @csrf
+        @method('DELETE')
+    </form>
+</x-layout>
