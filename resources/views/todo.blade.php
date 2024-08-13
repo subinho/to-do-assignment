1f50cb3164dbcd2a3df500ac7044b8531047a535
unknown <stepansubrt@icloud.com>
2024-08-14 00:13:15 +0200

    showing a todo

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,4 +1,23 @@
 <x-layout>
     <h3>{{$todo->title}}</h3>
     <p>{{$todo->body}}</p>
+
+    <div class="mt-6 flex items-center justify-between gap-x-6">
+        <form method="POST" action="/todo/{{ $todo->id }}" id="delete-form">
+            @csrf
+            @method('DELETE')
+
+            <div class="flex items-center">
+                <button form="delete-form" class="text-red-500 text-sm font-bold">Delete</button>
+            </div>
+        </form>
+
+        <div class="flex items-center gap-x-6">
+            <a href="/" class="text-sm font-semibold leading-6 text-gray-900">Go back</a>
+
+            <div>
+                <x-button href="/todo/{{ $todo->id }}/edit">Edit Job</x-button>
+            </div>
+        </div>
+    </div>
 </x-layout>
