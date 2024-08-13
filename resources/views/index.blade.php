5fcb7de1bc447445faa5f0e1e7ff208e61815831
unknown <stepansubrt@icloud.com>
2024-08-13 23:35:27 +0200

    added edit and delete html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,14 @@
 <x-layout>
     @foreach($todos as $todo)
-        <div>
-            <h3>{{$todo->title}}</h3>
-            <p>{{$todo->body}}</p>
+        <div class="mb-4 flex justify-between items-center">
+            <a href="/todo/{{$todo->id}}">
+                <h3 class="text-lg font-bold">{{$todo->title}}</h3>
+                <p class="text-sm">{{$todo->body}}</p>
+            </a>
+            <div class="flex items-end">
+                <a href="/todo/edit" class="mr-2">Edit</a>
+                <button type="submit">Delete</button>
+            </div>
         </div>
     @endforeach
 </x-layout>
