3aa0a3b889361f6e13a27999e272cfe3763bd322
unknown <stepansubrt@icloud.com>
2024-08-14 20:01:24 +0200

    calculating remaining time to do the task

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -9,10 +10,30 @@ class Todo extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'body', 'completed'];
+    protected $fillable = ['title', 'body', 'completed', 'user_id'];
 
     public function users()
     {
         $this->belongsTo(User::class);
     }
+
+    public function getRemainingTime()
+    {
+        $createdAt = Carbon::parse($this->created_at);
+        $expiresAt = $createdAt->addHours(24);
+        $now = Carbon::now();
+
+        $remainingMinutes = round(abs($expiresAt->diffInMinutes($now)));
+        $remainingHours = round(abs($expiresAt->diffInHours($now)));
+
+        if ($expiresAt->isPast()) {
+            return 'Expired';
+        } elseif ($remainingHours <= 1) {
+            return $remainingMinutes . ' ' . ($remainingMinutes > 1 ? 'minutes' : 'minute');
+        } else {
+            return $remainingHours . ' hours';
+        }
+    }
+
+
 }
