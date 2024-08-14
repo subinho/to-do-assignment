16d3e3eeb0a19c23f4a7aba5084b54db9c69df61
unknown <stepansubrt@icloud.com>
2024-08-14 23:35:54 +0200

    started production

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -9,7 +9,8 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite(['resources/css/app.css'])
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
 
     <!-- Styles -->
     <style>
