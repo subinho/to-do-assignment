98f0cfa5c0750ecec6b63ba998e2f2c1b4cd1fac
unknown <stepansubrt@icloud.com>
2024-08-14 02:19:28 +0200

    Log in to account and regenerate session token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,12 +5,31 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
     public function create()
     {
         return view('auth/login');
+    }
+
+    public function store()
+    {
+        $validated = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required']
+        ]);
+
+        if(! Auth::attempt($validated, true)){
+            throw ValidationException::withMessages([
+                'email' => 'Those credentials do not match!'
+            ]);
+        }
+
+        request()->session()->regenerate();
+
+        return redirect('/');
     }
 
     public function destroy()
--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,5 @@ Route::get('/register', [RegisterController::class, 'create']);
 Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'create']);
+Route::post('login', [LoginController::class, 'store']);
 Route::post('/logout', [LoginController::class, 'destroy']);
